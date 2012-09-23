f08be679f8bab42e5b794585f0192ebdce3682bc
Evan Kuchar <evan.kuchar@gmail.com>
2012-09-23 15:19:35 -0500

    change cookie domain again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OSP::Application.config.session_store :cookie_store, key: '_OSP_session'
+OSP::Application.config.session_store :cookie_store, key: '_OSP_session', :domain => "ourstoryproject.org"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
