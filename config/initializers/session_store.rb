293f52239bacd95f85cc8feb1f6df55e78babfe6
Evan Kuchar <evan.kuchar@gmail.com>
2012-09-23 18:56:25 -0500

    fix title on show

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
-
-OSP::Application.config.session_store :cookie_store, key: '_OSP_session', :domain => "ourstoryproject.org"
-
+if ENV["RAILS_ENV"] == "production"
+  OSP::Application.config.session_store :cookie_store, key: '_OSP_session', :domain => "ourstoryproject.org"
+else
+  OSP::Application.config.session_store :cookie_store, key: '_OSP_session' #, :domain => "ourstoryproject.org"
+end
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
