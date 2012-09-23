55e547b26c91bcb5626c1dd73c1be83ce61b1f63
Evan Kuchar <evan.kuchar@gmail.com>
2012-09-23 15:14:29 -0500

    change cookie session store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ OSP::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.session_store :cookie_store, key: '_OSP_session', :domain => "ourstoryproject.org"
   config.action_mailer.default_url_options = { :host => 'ourstoryproject.org' }
   
 end
