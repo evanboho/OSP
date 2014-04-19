f96afc0a523db2dce1a91690fa2c33cab069d677
evanboho <evan.kuchar@gmail.com>
2014-04-19 16:29:53 -0700

    add registerable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable, :registerable, :validatable
-  devise :database_authenticatable,
+  # :lockable, :timeoutable and :omniauthable, :validatable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
   validates_uniqueness_of :email
