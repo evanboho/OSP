abfe4515ebc8c72df596bdf159c31d1c1be5c412
evanboho <evan.kuchar@gmail.com>
2014-04-19 16:45:36 -0700

    rails 4 updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OSP::Application.config.secret_token = 'aea374dfcf150483da996007c20da685721c59c10ffde18d9acb9b450e0cb04979711a75a248a6530c1f244a67bcdea0bb591143217f9eb8a777671f9e3d9069'
+OSP::Application.config.secret_key_base = 'aea374dfcf1504d83da996007c20da685721c59c10ffde18d9facb9b450e0cb04979711a75a248a6530dc1f244a67bcdea0bb591143217f9eb8a7f77671f9e3d9069'
