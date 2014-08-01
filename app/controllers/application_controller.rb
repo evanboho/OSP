33c06ddd8a41738de9ed38d556b07ae8e304acba
evanboho <evan.kuchar@gmail.com>
2014-08-01 10:34:57 -0700

    iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   after_action :allow_iframe
 
   def allow_iframe
-    response.headers.except! 'X-Frame-Options'
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://ourstoryproject.org/'
   end
 
   helper_method :current_user, :current_user?
