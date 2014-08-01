9c37fc1964aba3892f2d6349ee7b1cdf421b2929
evanboho <evan.kuchar@gmail.com>
2014-08-01 10:44:43 -0700

    add secret key base

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,6 +4,8 @@ if ENV["RAILS_ENV"] == "production"
 else
   OSP::Application.config.session_store :cookie_store, key: '_OSP_session' #, :domain => "ourstoryproject.org"
 end
+
+OSP::Application.config.secret_key_base = '0ff38ebf08bd88fd08ba4d63459c6c84220c6622ced29a9cd172ac57b72008cb6bcd5e448d7c2d8775c6ad5122a5ded4d7f506ec22720bf6a15100e2560a332a'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
